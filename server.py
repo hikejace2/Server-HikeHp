43b4f3f0bae31a4fd5316e19f35caae7cdab09cd
hikejace2 <hikerekkind@gmail.com>
2024-07-14 23:35:02 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -16,18 +16,12 @@
 # sa		      Sudamérica (São Paulo)
 # us		      Estados unidos (Ohio)
 # us-cal-1	      Estados unidos (California)
-
-
-
-
-
-
-
-
-
-
-
-
+  
+      
+            
+                     
+  
+   
 
 
 # No toques nada de aquí para abajo, puedes dañarlo
